959f5f449e07850505d4e14f6cf3b0a27c801eb8
Caio Melo <caio.melo@toptal.com>
2023-11-20 16:09:44 -0300

    Set up a GraphQL server with GraphQL Yoga, set up the Pothos schema builder, defined your GraphQL object and query types and queried for data using Prisma Client

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hey there! 👋');
+import { createYoga } from "graphql-yoga";
+import { createServer } from "node:http";
+import { schema } from "./schema";
+
+const port = Number(process.env.API_PORT) || 4000;
+
+// Create a Yoga instance with a GraphQL schema.
+const yoga = createYoga({ schema });
+
+// Pass it into a server to hook into request handlers.
+const server = createServer(yoga);
+
+server.listen(port, () => {
+  console.log(`🚀 GraphQL Server ready at http://localhost:${port}/graphql`);
+});
